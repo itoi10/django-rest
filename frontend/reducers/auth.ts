26b53307bf63e48aaa61c742059f9431dd65a07c
itict10 <itict10@gmail.com>
2021-12-30 22:14:09 +0900

    add case to authReducer

--- frontend/reducers/auth.ts
+++ frontend/reducers/auth.ts
@@ -1,3 +1,5 @@
+import { REGISTER_FAIL, REGISTER_SUCCESS, REMOVE_AUTH_LOADING, SET_AUTH_LOADING } from "../actions/types"
+
 // 初期状態
 const initialState = {
   user: null,
@@ -9,6 +11,26 @@ const authReducer = (state = initialState, action) => {
   const {type, payload} = action
 
   switch (type) {
+    // アカウント登録
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+      }
+    case REGISTER_FAIL:
+      return {
+        ...state,
+      }
+    // 読込中
+    case SET_AUTH_LOADING:
+      return {
+        ...state,
+        loading: true,
+      }
+    case REMOVE_AUTH_LOADING:
+      return {
+        ...state,
+        loading: false,
+      }
 
     default:
       return state
