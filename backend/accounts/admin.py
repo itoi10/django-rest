a16d71b6321256e3247a05b606b7945a9a068843
itict10 <itict10@gmail.com>
2022-01-17 23:12:06 +0900

    add UserAdmin class

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,8 +1,57 @@
+from dataclasses import field
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 # 管理画面の設定
 
 # ユーザーモデル取得
 User = get_user_model()
+
+# 管理画面表示
+
+
+class UserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {
+            'fields': (
+                'name',
+                'email',
+                'password',
+                'image',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
+        })
+    )
+
+    # 管理画面からユーザー登録
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'name',
+                'email',
+                'password1',
+                'password2',
+                'image',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        })
+    )
+
+    list_display = (
+        'id',
+        'name',
+        'email',
+        'updated_at',
+        'created_at',
+    )
+
+    list_display_links = ('id', 'name', 'email')
+    ordering = ('id',)
+
+
 # 登録
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
