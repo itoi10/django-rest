e25034584596b7377247835bda659154de375a91
itict10 <itict10@gmail.com>
2022-01-20 21:36:23 +0900

    add post model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+# 画像投稿のモデル
+class Post(models.Model):
+    # リレーション on_delete=によりuser削除でに紐づいたpostも削除される挙動になる
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    title = models.CharField('タイトル', max_length=200)
+    image = models.ImageField(upload_to='images', verbose_name='画像', null=True, blank=True)
+    content = models.TimeField('本文')
+    updated_at = models.DateTimeField('更新日', auto_now=True)
+    created_at = models.DateTimeField('作成日': auto_now_add=True)
