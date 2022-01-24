c6178cdcc35402c9332f83e8dacccdb2d49e63c4
itict10 <itict10@gmail.com>
2022-01-24 22:05:53 +0900

    fix post model

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,12 +5,11 @@ from django.conf import settings
 class Post(models.Model):
     # リレーション on_delete=によりuser削除でに紐づいたpostも削除される挙動になる
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
     title = models.CharField("タイトル", max_length=200)
     image = models.ImageField(
         upload_to="images", verbose_name="画像", null=True, blank=True
     )
-    content = models.TimeField("本文")
+    content = models.TextField("本文")
     updated_at = models.DateTimeField("更新日", auto_now=True)
     created_at = models.DateTimeField("作成日", auto_now_add=True)
 
