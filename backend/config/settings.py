1154b6508e259d9958660be65019b1e48f60f61f
itict10 <itict10@gmail.com>
2022-01-27 21:05:23 +0900

    exclude SECRET_KEY from git

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from .local_settings import SECRET_KEY
 from pathlib import Path
 from datetime import timedelta
 
@@ -19,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-b)-%qq^qqo!1=a=bm09m5kh!dt4e-ehv=-uac&wl88)@2(pjl5"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
