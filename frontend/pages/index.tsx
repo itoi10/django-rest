8a202560b8aee5d8caa76be49138dd3302353748
itict10 <itict10@gmail.com>
2022-02-01 07:42:55 +0900

    (nuxt) change index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,36 +1,128 @@
-import React from 'react'
-import Head from 'next/head'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useRouter } from "next/router";
+import { getPostList } from '../lib/posts'
+import useSWR from "swr";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
 
-const Index:React.FC = () => {
-  const isAuthenticated = useSelector((state:any) => state.auth.isAuthenticated)
+
+// SWR用fetcher関数. URLをフェッチしてJSON形式で返す
+const fetcher = (url:string) => fetch(url).then((res) => res.json())
+
+const Index:React.FC = ({staticPosts}:any) => {
+  const dispatch = useDispatch()
+  const router = useRouter()
   const user = useSelector((state:any) => state.auth.user)
+
+  // SWR データ取得のための React Hooks ライブラリ
+  // https://swr.vercel.app/ja/docs/getting-started
+  const { data: posts, mutate } = useSWR(
+    // URL
+    `${process.env.NEXT_PUBLIC_API_URL}/api/post_list/`,
+    // fetcher関数
+    fetcher,
+    // SSRやSSGではuseSWRでリクエスト前にデータを取得していることがあるので、fallbackDataであらかじめ初期値に設定できる
+    {
+      fallbackData: staticPosts,
+    }
+  )
+
+  // 最新データ取得
+  useEffect(() => {
+    mutate()
+  }, [])
+
+  // 投稿が取得でき慣れけばLoadingテキスト表示
+  if (router.isFallback || !posts) {
+    return <div className="text-center">Loading...</div>
+  }
 
   return (
     <>
       <Head>
-        <title>Django - Next.js Sample</title>
+        <title>DjangoREST Next.js App</title>
       </Head>
-      <div>
-        {isAuthenticated && user ? (
-          <div>
-          <div>
-            <div>ようこそ、{user.name}さん</div>
+
+      <div className="max-w-screen-lg mx-auto">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-2">
+            {/* 投稿データ表示 */}
+            {posts && posts.map((data) => (
+              <div className="border mb-6 bg-white" key={data.id}>
+                <div className="flex items-center space-x-4 p-4">
+                  {/* 投稿者プロフィール画像 */}
+                  <Image
+                    src={data.user.image}
+                    className="rounded-full"
+                    alt={data.user.name}
+                    width={40}
+                    height={40}
+                    objectFit="cover"
+                  />
+                  <div>
+                    {/* 投稿者名 */}
+                    <div>{data.user.name}</div>
+                    {/* タイトル */}
+                    <div>{data.title}</div>
+                  </div>
+                </div>
+                <div>
+                  {/* 投稿画像 (投稿詳細へのリンク) */}
+                  <Link href={`/post/${data.id}`}>
+                    <a>
+                      <Image
+                        src={data.image}
+                        className=""
+                        alt={data.title}
+                        width={1000}
+                        height={1000}
+                        objectFit="cover"
+                      />
+                    </a>
+                  </Link>
+                </div>
+                <div className="m-4">
+                  {/* 投稿者名 */}
+                  <div>{data.user.name}</div>
+                  {/* 説明文 */}
+                  <div className="truncate">{data.content}</div>
+                </div>
+              </div>
+            ))}
           </div>
-          <div className="my-4 border-4 border-dashed border-gray-200 rounded">
-            <div className="flex justify-center items-center h-64">
-              コンテンツ１
-            </div>
+          {/* ログイン中であればプロフィール表示 */}
+          <div className="col-span-1">
+            {user && (
+              <div className="flex items-center space-x-4">
+                <Image
+                  src={user.image}
+                  className="rounded-full"
+                  alt={user.name}
+                  width={50}
+                  height={50}
+                  objectFit="cover"
+                />
+                <div>{user.name}</div>
+              </div>
+            )}
           </div>
-          </div>
-        ) : (
-          <div className="text-center text-2xl">
-            Django and Next.js App
-          </div>
-        )}
+        </div>
       </div>
     </>
   )
 }
 
 export default Index
+
+// SSG
+export async function getStaticProps() {
+  const staticPosts = await getPostList()
+
+
+  return {
+    props: {staticPosts},
+    revalidate: 1,
+  }
+}
